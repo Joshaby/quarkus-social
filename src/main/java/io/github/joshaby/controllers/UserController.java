d95ec45e2042a09b8dbb343613e3ee4c2eb84a97
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2024-11-27 12:07:13 -0300

    Adiciona métodos de atualizar e remove usuário. Atualiza método de criar usuário

--- src/main/java/io/github/joshaby/controllers/UserController.java
+++ src/main/java/io/github/joshaby/controllers/UserController.java
@@ -6,6 +6,9 @@ import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+
+import java.net.URI;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -20,11 +23,34 @@ public class UserController {
         user.setAge(request.age());
         user.persist();
 
-        return Response.ok(user).build();
+        URI uri = UriBuilder.fromResource(UserController.class).path("/{id}").build(user.getId());
+        return Response.created(uri).build();
     }
 
     @GET
     public Response listAll() {
         return Response.ok(User.listAll()).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response update(@PathParam("id") Long id, UserRequest request) {
+        return User.findByIdOptional(id).map(user -> {
+            ((User) user).setName(request.name());
+            ((User) user).setAge(request.age());
+
+            return Response.noContent().build();
+        }).orElse(Response.status(Response.Status.NOT_FOUND).build());
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response delete(@PathParam("id") Long id) {
+        return User.findByIdOptional(id).map(user -> {
+            user.delete();
+            return Response.noContent().build();
+        }).orElse(Response.status(Response.Status.NOT_FOUND).build());
     }
 }

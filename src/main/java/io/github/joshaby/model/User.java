92b2df410baed50a9d297b0d133c13d3a3b681d8
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2024-11-27 00:24:39 -0300

    Adiciona entidade User

---
+++ src/main/java/io/github/joshaby/model/User.java
@@ -0,0 +1,22 @@
+package io.github.joshaby.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "USERS")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+}
